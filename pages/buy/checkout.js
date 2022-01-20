c3d30535fdd9b11948d0484f5133a73428979936
Martin Zannato <martin.zannato@eztitles.com>
2022-01-20 10:55:50 +0200

    minor shit

--- pages/buy/checkout.js
+++ pages/buy/checkout.js
@@ -208,7 +208,7 @@ export default function Checkout() {
             </div>
           )}
           <div className="billing__inner-step">
-            <h2 className="billing-step__title">whatever next step is</h2>
+            <h2 className="billing-step__title">Hardware ID</h2>
           </div>
         </div>
         <div className="cart">
@@ -256,58 +256,6 @@ export default function Checkout() {
           </div>
         </div>
       </section>
-      {/*   {checkoutItems.length > 0 ? (
-        <section className="checkout">
-          <div className="billing">
-            <div className="billing__inner">
-              <h2 className="billing__title">Welcome to EZTitles store</h2>
-              <h2 className="billing__description">
-                Please, log-in in your profile or continue as new customer
-              </h2>
-              <div className="billing__buttons">
-                <a href="#" className="button button_basic_long">
-                  LOG IN
-                </a>
-                <a href="#" className="button button_basic_long_inverted">
-                  NEW CUSTOMER
-                </a>
-              </div>
-            </div>
-            <div className="billing__inner-step">
-              <h2 className="billing-step__title">whatever next step is</h2>
-            </div>
-          </div>
-          <div className="cart">
-            <div className="cart__inner">
-              <div className="cart__title-section">
-                <h2 className="cart__title">Order Summary</h2>
-                <span className="cart__edit">Edit</span>
-              </div>
-              {items}
-              <div className="cart__sum">
-                <h5 className="cart__sum__title">
-                  subtotal: {totalPrices.subtotal}
-                  <sup> EUR</sup>
-                </h5>
-                <h5 className="cart__sum__title">
-                  VAT 25%: {totalPrices.vat}
-                  <sup> EUR</sup>
-                </h5>
-                <h4 className="cart__sum__title-total">
-                  TOTAL: {totalPrices.total} <sup> EUR</sup>
-                </h4>
-              </div>
-            </div>
-          </div>
-        </section>
-      ) : (
-        <section className="checkout checkout-empty">
-          <h1 className="checkout-empty__h1">EMPTY CHECKOUT</h1>
-          <Link href="/">
-            <a className="checkout-empty__link">Back to home</a>
-          </Link>
-        </section>
-      )}*/}
     </Layout>
   );
 }
