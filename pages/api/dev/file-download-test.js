ea92db790ee491dad517292bf57fcc765a8dcf47
Martin Zannato <martin.zannato@eztitles.com>
2022-03-01 09:26:12 +0200

    fixing lottie

--- pages/api/dev/file-download-test.js
+++ pages/api/dev/file-download-test.js
@@ -1,10 +1,8 @@
 const fs = require("fs").promises;
 const https = require("https");
 
-
 export default async function handler(req, res) {
   const filePath = "https://nextjsstorage.blob.core.windows.net/test-files/";
   const fileName = req.body.name;
   const uri = filePath.concat(fileName);
-
 }
