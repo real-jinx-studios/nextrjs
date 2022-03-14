be7748f78295ade4e0bdd08c307e1e706a8dde61
Martin Zannato <martin.zannato@eztitles.com>
2022-03-14 15:26:36 +0200

    home page stuff more

--- components/home_page/products_showcase.js
+++ components/home_page/products_showcase.js
@@ -2,13 +2,18 @@ import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 
 export default function ProductsShowcase() {
-  const [selectedProduct, setSelectedProduct] = useState("eztitles");
-  const handleSelectProductChange = (e) => {
-    setSelectedProduct(e.target.value);
-  };
+  const [selectedProduct, setSelectedProduct] = useState("blue");
+
   const oneRef = useRef();
   const twoRef = useRef();
   const threeRef = useRef();
+  const handleSelectProductChange = (e, value) => {
+    console.log(e, value);
+    setSelectedProduct(value);
+  };
+  useEffect(() => {
+    console.log("selected product", selectedProduct);
+  }, [selectedProduct]);
   useEffect(() => {
     oneRef.current.classList.add("active");
     twoRef.current.classList.add("active");
@@ -18,7 +23,7 @@ export default function ProductsShowcase() {
   return (
     <section className="section wide">
       <style jsx>{`
-        .main {
+        .main_inner {
           margin: 0 auto;
           display: flex;
           width: 100%;
@@ -70,53 +75,113 @@ export default function ProductsShowcase() {
           border-color: #fbb03b;
         }
 
-        #green:checked ~ .one.active {
+        #blue:checked ~ .main > .main_inner > .one.active {
           transform: translateX(100%);
           z-index: 2;
         }
-        #green:checked ~ .two.active {
+        #blue:checked ~ .main > .main_inner > .two.active {
           transform: translateX(-40%) scale(0.9);
         }
-        #green:checked ~ .three.active {
+        #blue:checked ~ .main > .main_inner > .three.active {
           transform: translateX(-60%) scale(0.9);
         }
-        #red:checked ~ .one.active {
+        #red:checked ~ .main > .main_inner > .one.active {
           transform: translateX(60%) scale(0.9);
         }
-        #red:checked ~ .two.active {
+        #red:checked ~ .main > .main_inner > .two.active {
           transform: translateX(0) scale(1);
           z-index: 2;
         }
-        #red:checked ~ .three.active {
+        #red:checked ~ .main > .main_inner > .three.active {
           transform: translateX(-60%) scale(0.9);
         }
-        #purple:checked ~ .one.active {
+        #orange:checked ~ .main > .main_inner > .one.active {
           transform: translateX(60%) scale(0.9);
         }
-        #purple:checked ~ .two.active {
+        #orange:checked ~ .main > .main_inner > .two.active {
           transform: translateX(40%) scale(0.9);
         }
-        #purple:checked ~ .three.active {
+        #orange:checked ~ .main > .main_inner > .three.active {
           transform: translateX(-100%) scale(1);
           z-index: 2;
         }
-        #green,
+        #blue,
         #red,
-        #purple {
+        #orange {
           position: absolute;
           top: 0;
           left: 0;
           display: none;
         }
-        #green {
+        #blue {
           top: 50%;
         }
         #red {
           top: 55%;
         }
-        #purple {
+        #orange {
           top: 60%;
         }
+
+        .ezt,
+        .ezc,
+        .ezp {
+          position: absolute;
+          transition: all 0.3s ease-in-out;
+        }
+        .ezt {
+          transform: translateX(0);
+        }
+        .ezc {
+          transform: translateX(-150%);
+        }
+        .ezp {
+          transform: translateX(-250%);
+        }
+
+        #blue:checked ~ .details_container > .ezt {
+          transform: translateX(0);
+          z-index: 2;
+        }
+        #blue:checked ~ .details_container > .ezc {
+          transform: translateX(-150%);
+          opacity: 0;
+        }
+        #blue:checked ~ .details_container > .ezp {
+          transform: translateX(-250%);
+          opacity: 0;
+        }
+        #red:checked ~ .details_container > .ezt {
+          transform: translateX(-150%);
+          z-index: 1;
+          opacity: 0;
+        }
+        #red:checked ~ .details_container > .ezc {
+          transform: translateX(0);
+          z-index: 2;
+          opacity: 1;
+        }
+        #red:checked ~ .details_container > .ezp {
+          transform: translateX(-250%);
+          z-index: 1;
+          opacity: 0;
+        }
+        #orange:checked ~ .details_container > .ezt {
+          transform: translateX(-150%);
+          z-index: 1;
+          opacity: 0;
+        }
+        #orange:checked ~ .details_container > .ezc {
+          transform: translateX(-250%);
+          z-index: 1;
+          opacity: 0;
+        }
+        #orange:checked ~ .details_container > .ezp {
+          transform: translateX(0);
+          z-index: 2;
+          opacity: 1;
+        }
+
         .main_wrapper {
           display: flex;
           justify-content: space-between;
@@ -169,10 +234,12 @@ export default function ProductsShowcase() {
           flex: 0 0 34%;
           padding: 2em;
           margin: 0 auto;
+          position: relative;
         }
-        .details_products {
+        .details_product {
+          width: 80%;
         }
-        .details_products.active {
+        .details_product.active {
         }
         .details_title_wrapper {
           text-align: center;
@@ -192,8 +259,32 @@ export default function ProductsShowcase() {
         }
       `}</style>
       <div className="main_wrapper">
+        <input
+          type="radio"
+          name="switch"
+          id="blue"
+          onClick={(e) => {
+            handleSelectProductChange(e, "blue");
+          }}
+        />
+        <input
+          type="radio"
+          name="switch"
+          id="red"
+          onClick={(e) => {
+            handleSelectProductChange(e, "red");
+          }}
+        />
+        <input
+          type="radio"
+          name="switch"
+          id="orange"
+          onClick={(e) => {
+            handleSelectProductChange(e, "orange");
+          }}
+        />
         <div className="details_container">
-          <div className="details_product">
+          <div className="details_product ezt">
             <div className="details_title_wrapper">
               <h3 className="details_title">
                 Subtitles and Closed Captions creation software
@@ -206,24 +297,62 @@ export default function ProductsShowcase() {
             </div>
             <div className="details_buttons">
               <Link href="/products/free-trial">
-                <a className="button button_basic_long">FREE TRIAL</a>
+                <a className="button button_basic_long blue">FREE TRIAL</a>
               </Link>
               <Link href="/checkout">
-                <a className="button button_basic_long">BUY NOW</a>
+                <a className="button button_basic_long blue">BUY NOW</a>
               </Link>
               <Link href="/products/ezt/license">
-                <a className="button button_basic_long">LICENSE EDITIONS</a>
+                <a className="button button_basic_long blue">
+                  LICENSE EDITIONS
+                </a>
+              </Link>
+            </div>
+          </div>
+          <div className="details_product ezc">
+            <div className="details_title_wrapper">
+              <h3 className="details_title">Subtitles conversion software</h3>
+              <p className="details_subtitle">
+                <strong>Starting from €80/month</strong>
+                <br />
+                <em>for EZConvert GUI</em>
+              </p>
+            </div>
+            <div className="details_buttons">
+              <Link href="/products/free-trial">
+                <a className="button button_basic_long red">FREE TRIAL</a>
+              </Link>
+              <Link href="/checkout">
+                <a className="button button_basic_long red">BUY NOW</a>
+              </Link>
+              <Link href="/products/ezt/license">
+                <a className="button button_basic_long red">LICENSE EDITIONS</a>
+              </Link>
+            </div>
+          </div>
+          <div className="details_product ezp">
+            <div className="details_title_wrapper">
+              <h3 className="details_title">Plugins for EZTitles</h3>
+              <p className="details_subtitle">
+                <strong>Starting from €60/month</strong>
+                <br />
+                <em>for all plugins</em>
+              </p>
+            </div>
+            <div className="details_buttons">
+              <Link href="/products/free-trial">
+                <a className="button button_basic_long orange">FREE TRIAL</a>
+              </Link>
+              <Link href="/checkout">
+                <a className="button button_basic_long orange">BUY NOW</a>
               </Link>
             </div>
           </div>
         </div>
 
-        <div className="main_inner">
-          <div className="main">
-            <input type="radio" name="switch" id="green" />
-            <input type="radio" name="switch" id="red" />
-            <input type="radio" name="switch" id="purple" />
-            <label ref={oneRef} className="one card" htmlFor="green">
+        <div className="main">
+          <div className="main_inner">
+            <label ref={oneRef} className="one card" htmlFor="blue">
               <div className="card_product_platforms">
                 <img width={45} height={45} src="/images/windows.png" />
               </div>
@@ -267,7 +396,7 @@ export default function ProductsShowcase() {
                 </Link>
               </div>
             </label>
-            <label ref={threeRef} className="three card" htmlFor="purple">
+            <label ref={threeRef} className="three card" htmlFor="orange">
               <div className="card_product_platforms">
                 <img width={45} height={45} src="/images/windows.png" />
               </div>
@@ -290,9 +419,9 @@ export default function ProductsShowcase() {
             </label>
           </div>
           <div className="main_controller">
-            <label htmlFor="green" />
+            <label htmlFor="blue" />
             <label htmlFor="red" />
-            <label htmlFor="purple" />
+            <label htmlFor="orange" />
           </div>
         </div>
       </div>
